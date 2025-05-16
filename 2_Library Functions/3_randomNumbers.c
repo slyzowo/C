46fee99732911dae8cdb9e8403e9bf23256d70b2
slyzOwO <lilithhimes@gmail.com>
2025-05-15 23:26:17 -0500

    Update 3_randomNumbers.c

--- 2_Library Functions/3_randomNumbers.c
+++ 2_Library Functions/3_randomNumbers.c
@@ -2,7 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 
+// Computer random numbers are not real random, they are pseudo-random (fake random)
+
 int main(){
+
+// you need a seed to generate random numbers, here we're using the time as a seed
+
+  srand(time(0));
+
+  int randomNumber1 = (rand() % 6) + 1; // random number = (random() % Maximum) + Minimum
+
+  printf("%d", randomNumber1);
 
 return 0;
 }
