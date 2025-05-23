da37d1b1e3688846a737064b9167569b8d023636
slyzOwO <lilithhimes@gmail.com>
2025-05-22 21:39:02 -0500

    Updating Pointers

--- 8_Memory/2_pointers.c
+++ 8_Memory/2_pointers.c
@@ -2,5 +2,14 @@
 
 int main(){
 
+  int age = 18;
+
+  int *pAge = &age; // integer pointerAge is equal to the address of age
+
+  printf("Address of age = %p\n", &age);
+  printf("Value of PAge = %p\n", pAge); // same thing
+
+  printf("Value stored at PAge = %d\n", *pAge); // dereferencing
+
 return 0;
 }
