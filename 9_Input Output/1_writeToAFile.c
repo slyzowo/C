e91697a54c806dcf24c07f6fb82b5cb9d765a9b1
slyzOwO <lilithhimes@gmail.com>
2025-05-23 20:35:33 -0500

    Update 1_writeToAFile.c

--- 9_Input Output/1_writeToAFile.c
+++ 9_Input Output/1_writeToAFile.c
@@ -2,5 +2,19 @@
 
 int main(){
 
+/*
+  w = Write
+  a = Append
+  r = Read
+*/
+
+  FILE *pF = fopen("test.txt", "w");
+
+  fprintf(pF, "Spongebob Squarepants");
+
+  fclose(pF);
+
+// makes a file called "Test.txt" writes "Spongebob Squarepants" and closes it
+
 return 0;
 }
