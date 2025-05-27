d743c03c377cab1a3c42b3b70e2eeb8b6802b878
slyzOwO <lilithhimes@gmail.com>
2025-05-27 18:18:03 -0500

    File!

--- 9_Input Output/2_readAFile.c
+++ 9_Input Output/2_readAFile.c
@@ -2,5 +2,14 @@
 
 int main(){
 
+  FILE *pF = fopen("test.txt", "r");
+
+  char buffer[255];
+
+  fgets(buffer, 255, pF);
+  printf("%s", buffer);
+
+  fclose(pF);
+
 return 0;
 }
--- 9_Input Output/3_deletingAFile.c
+++ 9_Input Output/3_deletingAFile.c
@@ -2,5 +2,13 @@
 
 int main(){
 
+if(remove("test.txt") == 0){
+  printf("That file has been deleted, thank you!");
+}
+
+else{
+  printf("That file was not deleted");
+}
+
 return 0;
 }
