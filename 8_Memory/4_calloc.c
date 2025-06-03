6f44d9ae8e856060028b69610f47c9b08735feac
slyzOwO <lilithhimes@gmail.com>
2025-06-02 23:54:47 -0500

    Update 4_calloc.c

--- 8_Memory/4_calloc.c
+++ 8_Memory/4_calloc.c
@@ -5,27 +5,7 @@ int main(){
 
   int number = 0;
 
-  printf("Enter the number of grades : \n");
-  scanf("%d", &number);
-
-  char *grades = malloc(number * sizeof(char));
-
-  if(grades == NULL){
-    printf("Memory allocation Failed!\n");
-    return 1;
-  }
-
-  for(int i = 0; i < number; i++){
-    printf("Enter grade #%d :\n", i + 1);
-    scanf(" %c", &grades[i]);
-  }
   
-  for(int i = 0; i < number; i++){
-    printf(" %c", grades[i]);
-  }
-
-  free(grades);
-  grades = NULL; 
 
 return 0;
 }
