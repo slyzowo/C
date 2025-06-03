4040811a2905c5742fb966b2515f613227c9d4ea
slyzOwO <lilithhimes@gmail.com>
2025-06-03 00:00:48 -0500

    Update 4_calloc.c

--- 8_Memory/4_calloc.c
+++ 8_Memory/4_calloc.c
@@ -4,8 +4,27 @@
 int main(){
 
   int number = 0;
+  printf("Enter the number of players : \n");
+  scanf("%d", &number);
 
+  int *scores = calloc(number, sizeof(int));
+
+  if(scores == NULL){
+    printf("Memory allocation Failed!\n");
+    return 1;
+  }
+
+  for(int i = 0; i < number; i++){
+    printf("Enter scores #%d :\n", i + 1);
+    scanf(" %c", &scores[i]);
+  }
   
+  for(int i = 0; i < number; i++){
+    printf(" %d", scores[i]);
+  }
+
+  free(scores);
+  scores = NULL; 
 
 return 0;
 }
