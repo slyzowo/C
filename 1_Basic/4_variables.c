d11083306648b467946d8584aa9227ed497ea0cc
slyzOwO <lilithhimes@gmail.com>
2025-06-09 11:34:52 -0500

    Update 4_variables.c

--- 1_Basic/4_variables.c
+++ 1_Basic/4_variables.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(){
 
@@ -12,11 +13,14 @@ int main(){
   float pi = 3.141592;      // decimal numbers
   char grade = 'C';         // character
   char name[] = "slyzOwO";  // string is a list of characters
+  bool student = true;      // a boolean is a True or False statement
+  
 
-  printf("you are %d years old \n", age);      // d = digit
-  printf("pi is %f \n", pi);                   // s = string
-  printf("my grade is %c \n", grade);          // c = character
-  printf("you\'re name is %s \n", name);       // s = string
+  printf("you are %d years old \n", age);         // d = digit
+  printf("pi is %f \n", pi);                      // s = string
+  printf("my grade is %c \n", grade);             // c = character
+  printf("you\'re name is %s \n", name);          // s = string
+  printf("you're a student :  %d \n", student);   // 1 = true, 0 = false we can get more advanced with this later
 
 return 0;
 }
