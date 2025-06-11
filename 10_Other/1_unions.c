da876bdc08f5bf7a2927a23f7bd0dadd37e96b1b
slyzOwO <lilithhimes@gmail.com>
2025-06-10 20:29:38 -0500

    more

---
+++ 10_Other/1_unions.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+union {
+
+};
+
+
+int main(){
+
+  
+
+return 0;
+}
---
+++ 10_Other/3_libraries.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "lib.c" // our library we're going to make
+
+int main(){
+
+  
+
+return 0;
+}
